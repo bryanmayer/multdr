1fd64670851eea7e2f5dd3686cd493e1d913504f
Bryan Mayer <msubryan@gmail.com>
2015-08-25 11:27:31 -0700

    fixed plot-surv survival NAMESPACE issues

--- R/plot-surv-output.R
+++ R/plot-surv-output.R
@@ -7,11 +7,10 @@
 #'
 #'@return a KM plot with multiple outputs
 
-
 plot_surv_output = function(data_KM, sim_data, rate_str = NULL, leg.x = 250, leg.y = 1){
-  survival::plot.survfit(data_KM)
+  plot(data_KM)
   if(any(is(sim_data) == "survfit")){
-    survival::lines.survfit(sim_data, conf.int = F, col = "red", mark.time = F, lty = 1)
+    lines(sim_data, conf.int = F, col = "red", mark.time = F, lty = 1)
     legend_text = c("Data", if(is.null(rate_str)) "Simulation" else rate_str)
     print(legend_text)
     legend(250, 1, legend_text, col = c("black", "red"), lty = 1)
@@ -21,8 +20,8 @@ plot_surv_output = function(data_KM, sim_data, rate_str = NULL, leg.x = 250, leg
     rate_str = NULL
     for(i in 1:length(rates)){
       sub_sim_data = subset(sim_data, rate == rates[i])
-      temp_surv = survival::survfit(Surv(time = detect_time, event = infected) ~ 1, data = sub_sim_data)
-      survival::lines.survfit(temp_surv, conf.int = F, col = plot_col[i], mark.time = F)
+      temp_surv = survival::survfit(survival::Surv(time = detect_time, event = infected) ~ 1, data = sub_sim_data)
+      lines(temp_surv, conf.int = F, col = plot_col[i], mark.time = F)
       rate_str = c(rate_str, paste("Rate = ", rates[i], "/day", sep = ""))
     }
     legend(leg.x, leg.y, c("Data", rate_str), col = c(1, plot_col[1:length(rates)]), lty = 1)
